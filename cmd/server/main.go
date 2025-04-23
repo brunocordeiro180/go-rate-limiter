ab3deb2802d684a20ee4fc7053fb6136158d7df8
brunocordeiro180 <brunocordeiro180@gmail.com>
2025-04-22 21:59:42 -0300

    Add rate limiter logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,33 @@
 package main
 
-import "github.com/brunocordeiro180/go-rate-limiter/internal/database"
+import (
+	"log"
+	"net/http"
+
+	"github.com/brunocordeiro180/go-rate-limiter/config"
+	"github.com/brunocordeiro180/go-rate-limiter/internal/infra/database"
+	middleware "github.com/brunocordeiro180/go-rate-limiter/internal/infra/webserver"
+	"github.com/brunocordeiro180/go-rate-limiter/internal/pkg/ratelimiter"
+)
 
 func main() {
-	redisConn := database.NewRedisConnection("localhost:6379", "", 0)
-	db := database.NewDatabaseConnector(redisConn)
-
-	if err := db.Connect(); err != nil {
+	cfg, err := config.LoadConfig(".")
+	if err != nil {
 		panic(err)
 	}
-	defer db.Disconnect()
+
+	storage := database.NewRedisConnection(cfg.RedisAddr, cfg.RedisPassword)
+	limiter := ratelimiter.NewRateLimiter(storage, cfg.RateLimitIP, cfg.RateLimitToken, cfg.RateDuration)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	port := ":8080"
+
+	handler := middleware.RateLimiterMiddleware(limiter, mux)
+
+	log.Println("Server running on port " + port)
+	log.Fatal(http.ListenAndServe(port, handler))
 }
